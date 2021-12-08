9858155969e0248ced4f9f8f9f11d1d29c97de73
vxsbh <73449404+vxsbh@users.noreply.github.com>
2021-12-08 22:35:06 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 echo "Lancement du Script..."
 sudo pacman -Syu
-sudo pacman -S curl fish
+sudo pacman -S curl fish autorandr neofetch
 clear
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git && makepkg -si
 cd
 yay -S brave-bin
+yay -S spotify
 
 git clone https://github.com/vxsbh/wallpapers
